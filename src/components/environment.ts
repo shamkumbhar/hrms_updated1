0c5cf9bca1c6e963700c81c20910ffe6da180d29
shamkumbhar <shamkumbhar47@gmail.com>
2025-07-15 18:56:37 +0530

    Update environment.ts

--- src/components/environment.ts
+++ src/components/environment.ts
@@ -7,7 +7,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api',
+  apiBaseUrl: 'http://13.203.221.94:8080/api',
   appName: 'HRMS',
   enableDebug: true
 };
